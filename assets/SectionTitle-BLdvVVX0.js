2adc18a2d2810bc6bfd89d9ab6101096e04ed035
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-06-19 20:25:43 +0200

    Updates

---
+++ assets/SectionTitle-BLdvVVX0.js
@@ -0,0 +1,1 @@
+import{j as e,h as i}from"./index-CR6L6q1L.js";const o="_section_title_186td_1",c={section_title:o},l=({text:t,addStyle:s=""})=>e.jsx("h2",{className:i(c.section_title,s),children:t});export{l as S};
---
+++ assets/recipe_without_img-BmqWHHrX.js
@@ -0,0 +1,1 @@
+const t="/foodies-front/assets/recipe_without_img-Bp4tHiOX.jpg";export{t as r};
